48f295b617f42ca3fd1e055297fef70cbe2a0066
jmudarra23 <155439082+jmudarra23@users.noreply.github.com>
2024-07-30 09:09:42 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,17 +6,17 @@ workspaces = {
         "api_key": st.secrets['API_KEY'],
         "labelers": {
             "lSjzzgbjkmNbcNh4xpae27ywdd92": "Elite Sports17",
-            "uy78ZKhgl9d2Dl1tV8v20Q6DbTb2": "José Enrique",
-            "l3bW925TA7hgB3e9XNcjTzJoPGB3": "01",
+            "EhV0M7z3ZRfzW6CLd7Ho2fKFmwe2": "Niel John Butad",
+            "l3bW925TA7hgB3e9XNcjTzJoPGB3": "Etiquetador SR",
         }
     },
     "socceranalytics2": {
         "subdomain": "socceranalytics2",
         "api_key": st.secrets['API_KEY2'],
         "labelers": {
+            "lSjzzgbjkmNbcNh4xpae27ywdd92": "Elite Sports17",
             "n7XjGcQyOURfX4CSmFX3Q617QBZ2": "Alessa Marce",
             "jSkhmVdFEzZCZDjbByrSpO7Ua3M2": "Victoria Bajo",
-            "lSjzzgbjkmNbcNh4xpae27ywdd92": "Elite Sports17",
         }
     }
 }
